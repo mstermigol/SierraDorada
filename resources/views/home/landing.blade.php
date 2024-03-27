4b4aec2479bc2008f0f508fb705220eeeb359e69
Miguel Jaramillo <migueljaramillo879@gmail.com>
2024-03-27 15:15:42 -0500

    refactor removed test code

--- resources/views/home/landing.blade.php
+++ resources/views/home/landing.blade.php
@@ -26,28 +26,4 @@
 
     </div>
 </section>
-
-<!-- About section -->
-<section id="about" class="d-flex align-items-center justify-content-center">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6">
-                <img src="{{ asset('img/about.jpg') }}" class="img-fluid" alt="About us">
-            </div>
-            <div class="col-lg-6 d-flex flex-column justify-content-center">
-                <h2>
-                    SOBRE NOSOTROS
-                </h2>
-                <p>
-                    Somos un club de chalanería para adultos y niños ubicado en el municipio de La Estrella. Nos dedicamos a fomentar el amor y respeto por los caballos.
-                </p>
-                <p>
-                    Ofrecemos clases de equitación, chalanería y equinoterapia. También organizamos eventos y actividades para toda la familia.
-                </p>
-                <a href="#0" class="btn btn-primary">Leer más</a>
-            </div>
-        </div>
-    </div>
-</section>
-
 @endsection
