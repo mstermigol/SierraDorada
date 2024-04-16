7103b99b0126e436611d8a782b908aefc14d8dcd
Miguel Jaramillo <migueljaramillo879@gmail.com>
2024-04-15 23:15:45 -0500

    Refactor paddings

--- resources/views/subviews/landing/hero.blade.php
+++ resources/views/subviews/landing/hero.blade.php
@@ -1,10 +1,10 @@
-<section id ="hero" class="d-flex align-items-center justify-content-start">
+<section id="hero" class="d-flex align-items-center justify-content-start">
     <div class="position-absolute top-0 container-fluid mt-3 mx-0 px-0">
         @include('subviews.navbar')
     </div>
-    
-    
-    <div class="container-fluid flex-column mx-5 hero-content-margin">
+
+
+    <div class="container-fluid flex-column mx-2 mx-sm-5 hero-content-margin">
 
         <h2 class="mb-0 pb-0 my-subtitle-letter">
             CLUB DE CHALANERIA
@@ -13,8 +13,8 @@
             SIERRA DORADA
         </h1>
         <P id="p-hero">
-        Somos un club de chalanería para adultos y niños ubicado en el municipio
-        de La Estrella. Nos dedicamos a fomentar el amor y respeto por los caballos.
+            Somos un club de chalanería para adultos y niños ubicado en el municipio
+            de La Estrella. Nos dedicamos a fomentar el amor y respeto por los caballos.
         </P>
         <button class="btn btn-golden mt-3 mb-5">
             <h2 class="text-white m-0 p-0">Inscríbete</h2>
--- resources/views/subviews/landing/services.blade.php
+++ resources/views/subviews/landing/services.blade.php
@@ -1,10 +1,12 @@
 <section class="container">
+    <h3>Nuestros</h3>
+    <h2>Servicios</h2>
     <div class="row g-5">
         <div class="col-md">
             <a href="#" class="card text-decoration-none h-100 my-service-card mx-auto">
                 <img src="{{ url("images/servicio1.jpg") }}" class="card-img-top  my-service-image" alt="Card Image">
                 <div class="card-body my-service-card-body">
-                    <h3 class="card-title my-subtitle-letter bold color-white text-center">Terapia asistida</h3>
+                    <h4 class="card-title my-subtitle-letter bold color-white text-center">Terapia asistida</h4>
                     <p class="card-text px-3 text-center">Ofrecemos un enfoque personalizado que integra la colaboracion
                         con los
                         caballos
@@ -16,7 +18,7 @@
             <a href="#" class="card text-decoration-none h-100 my-service-card mx-auto">
                 <img src="{{ url("images/servicio2.jpeg") }}" class="card-img-top my-service-image" alt="Card Image">
                 <div class="card-body my-service-card-body">
-                    <h3 class="card-title my-subtitle-letter bold color-white text-center">Chalanería Deportiva</h3>
+                    <h4 class="card-title my-subtitle-letter bold color-white text-center">Chalanería Deportiva</h4>
                     <p class="card-text px-3 text-center">Aprende el arte de la
                         chalaneria con nuestras
                         clases diseñadas para
@@ -28,7 +30,7 @@
             <a href="#" class="card text-decoration-none h-100 my-service-card mx-auto">
                 <img src="{{ url("images/servicio3.jpeg") }}" class="card-img-top my-service-image" alt="Card Image">
                 <div class="card-body my-service-card-body">
-                    <h3 class="card-title my-subtitle-letter bold color-white text-center">Estimulación Temprana</h3>
+                    <h4 class="card-title my-subtitle-letter bold color-white text-center">Estimulación Temprana</h4>
                     <p class="card-text px-3 text-center">Fomenta el desarrollo integral de tus pequeños, con nuestro
                         programa de
                         experiencias divertidas y educativas.</p>
