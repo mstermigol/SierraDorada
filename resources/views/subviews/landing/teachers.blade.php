dc3d172a750749eed0be81a2a7724903e14bd8ea
mstermigol <migueljaramillo879@gmail.com>
2024-05-02 00:53:28 -0500

    Add carousel
    Missing side nav buttons

--- resources/views/subviews/landing/teachers.blade.php
+++ resources/views/subviews/landing/teachers.blade.php
@@ -4,22 +4,22 @@
     <h2 class="my-title-letter my-section-title text-center mb-5" id="teachers-title">PROFESORES</h2>
 
     <!-- Teacher Container -->
-    <div class="container-fluid justify-content-around d-flex flex-row">
-        <div class="mb-4 mx-3">
+    <div class="owl-carousel owl-theme">
+        <div class="item">
             <div class="teacher-container text-center">
                 <img src="{{ url('images/hero-sierra.jpg') }}" class="image-circle" alt="Circle Image">
                 <p class="teacher-name text-dark mt-4 my-subtitle-letter my-section-subtitle teacher-name-spacing">Camilo Garcia Arbeláez</p>
             </div>
         </div>
 
-        <div class="mb-4 mx-3">
+        <div class="item">
             <div class="teacher-container text-center">
                 <img src="{{ url('images/hero-sierra.jpg') }}" class="image-circle" alt="Circle Image">
                 <p class="teacher-name text-dark mt-4 my-subtitle-letter my-section-subtitle teacher-name-spacing">Paula Rueda Uzuga</p>
             </div>
         </div>
 
-        <div class="mb-4 mx-3">
+        <div class="item">
             <div class="teacher-container text-center">
                 <img src="{{ url('images/hero-sierra.jpg') }}" class="image-circle" alt="Circle Image">
                 <p class="teacher-name text-dark mt-4 my-subtitle-letter my-section-subtitle teacher-name-spacing">Camila Henao Correa</p>
@@ -29,7 +29,38 @@
 
     <!-- Quote -->
     <div class="container-fluid d-flex justify-content-center mt-5 mb-4">
-        <p class="my-section-title color-gold quote">“Construyendo sueños al lomo del caballo”</p>
+        <p class="color-gold quote">“Construyendo sueños al lomo del caballo”</p>
     </div>
+
+
+    <script>
+        $(document).ready(function() {
+            $('.owl-carousel').owlCarousel({
+                
+                autoplay: true,
+                pagination: false,
+                center: true,
+                loop: true,
+                margin: 10,
+                nav: false,
+                dots: false,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    600: {
+                        items: 2
+                    },
+                    1000: {
+                        items: 3
+                    }
+
+                }
+                
+                
+            })
+        }
+        );
+    </script>
 
 </section>
