6057638ea8642f7771cd9b44710de279fa6c7d88
mstermigol <migueljaramillo879@gmail.com>
2024-05-06 17:19:30 -0500

    Add allies file and refactor carousel options

--- resources/views/subviews/landing/teachers.blade.php
+++ resources/views/subviews/landing/teachers.blade.php
@@ -1,4 +1,4 @@
-<section class="container-fluid pt-4 pb-3 px-0 relative"id="teachers-section">
+<section class="container-fluid pt-4 px-0 relative"id="teachers-section">
     <!-- Title -->
     <h3 class="my-subtitle-letter my-section-subtitle text-center" id="teachers-subtitle">CONOCE A NUESTROS</h3>
     <h2 class="my-title-letter my-section-title text-center mb-5" id="teachers-title">PROFESORES</h2>
