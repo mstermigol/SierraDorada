cf4b24664a4ccfc02e115fa11df611f0defc8d78
mstermigol <migueljaramillo879@gmail.com>
2024-05-07 05:07:53 -0500

    Add floating WhatsApp button to landing page

--- resources/views/home/landing.blade.php
+++ resources/views/home/landing.blade.php
@@ -1,25 +1,31 @@
-
 @extends('layouts.app')
 @section('title', 'Club Sierra Dorada | Equitación | Chalaneria | Equinoterapia | Eventos')
 @section('content')
 
 
-<!-- Hero section -->
-@include('subviews.landing.hero')
+  <!-- Hero section -->
+  @include('subviews.landing.hero')
 
-<!-- Services section -->
-@include('subviews.landing.services')
+  <!-- Services section -->
+  @include('subviews.landing.services')
 
-<!-- Teachers section -->
-@include('subviews.landing.teachers')
+  <!-- Teachers section -->
+  @include('subviews.landing.teachers')
 
-<!-- Testimonies section -->
-@include('subviews.landing.testimonies')
+  <!-- Testimonies section -->
+  @include('subviews.landing.testimonies')
 
-<!-- Allies section -->
-@include('subviews.landing.allies')
+  <!-- Allies section -->
+  @include('subviews.landing.allies')
 
-<!-- Footer section -->
-@include('subviews.footer')
+  <!-- Footer section -->
+  @include('subviews.footer')
+
+  <!-- Whatsapp button -->
+  <div class="fab">
+    <a href="#" target="_blank">
+      <img src="{{ url('images/whatsapp-fixed.png') }}" alt="WhatsApp Logo">
+    </a>
+  </div>
 
 @endsection
