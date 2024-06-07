3c9bd87d89a5f46f4ae2bc5d8c165d956a83b813
mstermigol <migueljaramillo879@gmail.com>
2024-06-07 15:23:09 -0500

    Refactor user model and add create and index for admin user
    In the user model I added the validation and the getters and setters

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,14 @@ Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.landing
 Route::middleware('auth')
     ->prefix('admin')
     ->group(function () {
-        Route::get('/usuarios', 'App\Http\Controllers\Admin\AdminUserController@index')->name('admin.user.index');
+        Route::prefix('usuarios')->group(function () {
+            Route::get('/', 'App\Http\Controllers\Admin\AdminUserController@index')->name('admin.user.index');
+            Route::get('/crear', 'App\Http\Controllers\Admin\AdminUserController@create')->name('admin.user.create');
+            Route::post('/guardar', 'App\Http\Controllers\Admin\AdminUserController@save')->name('admin.user.save');
+            Route::delete('/eliminar/{id}', 'App\Http\Controllers\Admin\AdminUserController@delete')->name('admin.user.delete');
+            Route::get('/editar/{id}', 'App\Http\Controllers\Admin\AdminUserController@edit')->name('admin.user.edit');
+            Route::post('/actualizar/{id}', 'App\Http\Controllers\Admin\AdminUserController@update')->name('admin.user.update');
+        });
     });
 
 Route::fallback(function () {
