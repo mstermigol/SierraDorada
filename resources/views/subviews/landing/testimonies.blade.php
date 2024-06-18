df502ed15794d157c51c0e33c413f51be9a20429
mstermigol <migueljaramillo879@gmail.com>
2024-06-18 00:03:24 -0500

    Refactor testimonies to use db

--- resources/views/subviews/landing/testimonies.blade.php
+++ resources/views/subviews/landing/testimonies.blade.php
@@ -6,29 +6,15 @@
 
         <!-- Testimonies -->
         <div class="row g-4 mb-4">
-            <div class="col">
-                <div class="my-testimony-card text-decoration-none text-center mx-auto">
-                    <img src="{{ url('images/servicio1.jpeg') }}" class="my-testimony-image mx-auto d-block" alt="Card Image">
-                    <h4 class="my-subtitle-letter bold text-light px-3">Daniela Bustamante</h4>
-                    <p class="px-3 text-light">“Estoy en las clases de chalanería desde que soy pequeña y es una de las mejores experiencias. Los profesores saben mucho del tema y me ayudan a dar lo mejor de mi .”</p>
-
+            @foreach ($viewData['testimonies'] as $testimony)
+                <div class="col">
+                    <div class="my-testimony-card text-decoration-none text-center mx-auto">
+                        <img src="{{ asset('storage/testimonies/' . $testimony->getImage()) }}" class="my-testimony-image mx-auto d-block" alt="Card Image">
+                        <h4 class="my-subtitle-letter bold text-light px-3">{{ $testimony->getName() }}</h4>
+                        <p class="px-3 text-light">{{ $testimony->getTestimony() }}</p>
+                    </div>
                 </div>
-            </div>
-            <div class="col">
-                <div class="my-testimony-card text-decoration-none text-center mx-auto">
-                    <img src="{{ url('images/servicio1.jpeg') }}" class="my-testimony-image mx-auto d-block" alt="Card Image">
-                    <h4 class="my-subtitle-letter bold text-light px-3">Carolina Osorio</h4>
-                    <p class="px-3 text-light">“Con mi hijo empecé la estimulación temprana y estoy muy feliz en la escuela. Los profes son siempre muy amables y mi hijo se emociona cada vez que vamos a clase. ”</p>
-
-                </div>
-            </div>
-            <div class="col">
-                <div class="my-testimony-card text-decoration-none text-center mx-auto">
-                    <img src="{{ url('images/servicio1.jpeg') }}" class="my-testimony-image mx-auto d-block" alt="Card Image">
-                    <h4 class="my-subtitle-letter bold text-light px-3">Esteban Molina</h4>
-                    <p class="px-3 text-light">“Siempre le tuve miedo a montar a caballo, pero desde que estoy en clases en Sierra he mejorado mucho y tengo más confianza. Descubrí lo mucho que me gustan los caballos. ”</p>
-
-                </div>
-            </div>
+            @endforeach
         </div>
+    </div>
 </section>
