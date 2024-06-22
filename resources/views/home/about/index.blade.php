0871b2166a902eddb0fb835f98cf5dbd32194358
mstermigol <migueljaramillo879@gmail.com>
2024-06-21 19:19:49 -0500

    Add about section

--- resources/views/home/about/index.blade.php
+++ resources/views/home/about/index.blade.php
@@ -2,28 +2,105 @@
 @section('title', 'Club Sierra Dorada Sobre Nosotros')
 @section('content')
 
-<div>
+  <div>
     <div class="my-vh-100">
-        <!-- Navbar -->
-        @include('subviews.navbarOther')
-        <div class="px-3 pt-4 pb-4 d-flex flex-column">
-            <div class="container-fluid p-0 mb-3 sections-arrow-title">
-                <a href="{{ route('home.landing') }}" class="btn btn-primary bg-gold b-gold mb-3">
-                    <i class="fas fa-arrow-left"></i>
-                </a>
-                <h2 class="my-title-letter my-section-title text-center container-fluid">NOSOTROS</h2>
-            </div>
-            <div class="row">
-                <div class="col-md-6 image-container-about">
-                    <img src="{{ asset('images/alejandra.jpg') }}" alt="Alejandra Correa" class="about-image">
-                    <p class="image-caption bold">*Alejandra Correa, nuestra directora</p>
-                </div>
-                <div class="col-md-6 about-text-container color-black">
-                    <p class="color-black">El club de chalanería Sierra Dorada, es una empresa netamente familiar la cual nació en el año 2015, y que durante este tiempo se ha dedicado a la crianza del caballo criollo colombiano, al manejo y conducción de dichos ejemplares; pero sobre todo a destacarse por ser una de las mejores escuelas de equitación, teniendo un nivel de competencia muy alto, el cual le permite llamar la atención dentro de todo el territorio nacional.</p>
-                </div>
-            </div>
+      <!-- Navbar -->
+      @include('subviews.navbarOther')
+      <div class="pt-4 pb-4 d-flex flex-column">
+        <div class="container-fluid p-0 mb-3 sections-arrow-title px-3">
+          <a href="{{ route('home.landing') }}" class="btn btn-primary bg-gold b-gold mb-3">
+            <i class="fas fa-arrow-left"></i>
+          </a>
+          <h2 class="my-title-letter my-section-title text-center container-fluid">NOSOTROS</h2>
         </div>
+        <div class="my-row px-3 bg-gold-light">
+          <div class="image-container-about">
+            <img src="{{ asset('images/alejandra.jpg') }}" alt="Alejandra Correa" class="about-image">
+            <p class="image-caption bold">*Alejandra Correa, nuestra directora</p>
+          </div>
+          <div class="about-text-container color-black">
+            <div class="about-text">
+              <p class="my-section-subtitle about-title text-center">
+                Nuestra Historia
+              </p>
+              <p class="color-black justify">El club de chalanería <strong>Sierra Dorada</strong>, es una empresa
+                netamente <strong>familiar</strong> la cual nació en
+                el año 2015, y que durante este tiempo se ha dedicado a la crianza del <strong>caballo criollo
+                  colombiano</strong>, al manejo
+                y conducción de dichos ejemplares; pero sobre todo a destacarse por ser una de las <strong>mejores
+                  escuelas de
+                  equitación</strong>, teniendo un nivel de competencia muy alto, el cual le permite llamar la atención
+                dentro de todo
+                el territorio nacional.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="my-row px-3 reverse">
+          <div class="about-text-container color-black">
+            <div class="about-text">
+              <p class="my-section-subtitle about-title text-center">
+                Nuestros Valores
+              </p>
+              <ul>
+                <li>Unión</li>
+                <li>Amor</li>
+                <li>Pasión</li>
+                <li>Respeto</li>
+                <li>Disciplina</li>
+                <li>Tolerancia</li>
+                <li>Sentido de pertenencia</li>
+              </ul>
+            </div>
+          </div>
+          <div class="image-container-about">
+            <img src="{{ asset('images/estudiante.jpeg') }}" alt="Sierra dorada" class="about-image">
+          </div>
+        </div>
+
+        <div class="my-row px-3 bg-gold-light">
+          <div class="image-container-about">
+            <img src="{{ asset('images/podio.jpg') }}" alt="Sierra dorada" class="about-image">
+          </div>
+          <div class="about-text-container color-black">
+            <div class="about-text">
+              <p class="my-section-subtitle about-title text-center">
+                Nuestra Misión
+              </p>
+              <p class="color-black justify">Promover la <strong>chalanería</strong> como un deporte, que forma
+                <strong>jinetes<strong> y </strong>amazonas</strong> íntegros
+                teniendo como base principal el <strong>respeto</strong> y <strong>amor</strong> por nuestro
+                <strong>caballo criollo colombiano.</strong>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="my-row px-3 reverse">
+          <div class="about-text-container color-black">
+            <div class="about-text">
+              <p class="my-section-subtitle about-title text-center">
+                Nuestra Visión
+              </p>
+              <p class="color-black justify">Ser una de las mejores <strong>escuelas de chalanería</strong> a nivel
+                nacional logrando
+                fomentar este <strong>deporte</strong> y enaltecer el <strong>caballo criollo colombiano.</strong></p>
+            </div>
+          </div>
+          <div class="image-container-about">
+            <img src="{{ asset('images/estudiante2.jpg') }}" alt="Sierra dorada" class="about-image">
+          </div>
+        </div>
+
+
+
+
+
+
+
+
+      </div>
     </div>
     @include('subviews.footer')
-</div>
+  </div>
 @endsection
