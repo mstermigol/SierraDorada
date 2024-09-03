d7c56435a059a07ced98b5d8f7c3d6a3a5a356b8
mstermigol <migueljaramillo879@gmail.com>
2024-09-02 22:50:43 -0500

    Refactor defer

--- resources/views/home/landing.blade.php
+++ resources/views/home/landing.blade.php
@@ -3,23 +3,14 @@
 @push('head')
   <meta name="description"
     content="Somos un club de chalanería para adultos y niños ubicado en el municipio de La Estrella. Nos dedicamos a fomentar el amor y respeto por los caballos." />
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
-        integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-      <!-- Owl Carousel Theme CSS -->
-      <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"
-        integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <!-- Diferir la carga de CSS no crítico -->
+  <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" as="style" onload="this.rel='stylesheet'">
+  <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" as="style" onload="this.rel='stylesheet'">
+
 @endpush
+
 @section('content')
-<!-- Owl Carousel js -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
-integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
-crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-
   <!-- Hero section -->
   @include('subviews.landing.hero')
 
@@ -41,6 +32,6 @@ crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <!-- Footer section -->
   @include('subviews.footer')
 
-
-
+  <!-- Scripts al final del body -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" defer crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 @endsection
